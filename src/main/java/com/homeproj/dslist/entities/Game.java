74c5cceb7058144f5b09cede7bfcc23810df6c85
Cristiano Araujo <crisd.silva75@gmail.com>
2025-05-21 14:59:56 -0300

    GameMinDTO, GameRepository, GameService, GameController

--- src/main/java/com/homeproj/dslist/entities/Game.java
+++ src/main/java/com/homeproj/dslist/entities/Game.java
@@ -15,7 +15,7 @@ public class Game {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	private String title;
 	
 	@Column(name = "game_year")
@@ -49,11 +49,11 @@ public class Game {
 		this.longDescription = longDescription;
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
---
+++ src/main/java/com/homeproj/dslist/repositories/GameRepository.java
@@ -0,0 +1,9 @@
+package com.homeproj.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.homeproj.dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+}
